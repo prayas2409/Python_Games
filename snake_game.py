e55243f6c99a55250a1f06781a94ce5732dbe449
prayas2409 <prayas.hemraj.pagade@gmail.com>
2020-12-07 12:07:37 +0530

    [Prayas]:Refactored to keep only code required for setting up window

--- snake_game.py
+++ snake_game.py
@@ -1,13 +1,11 @@
 import pygame
 
-window_height = 250 # as we'll have square so only 1 param is fine
-rows = 20
+window_height = 500 # as we'll have square so height = width hence 1 param is fine
 
 def main():
-    window = pygame.display.set_mode(size=(window_height,window_height))
-    clock = pygame.time.Clock()
+    window = pygame.display.set_mode(size=(window_height,window_height)) 
 
     while(True):
         pass
-    
+
 main()
