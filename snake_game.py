71fe2fc76d4b2f777917f9dd155fff4351fca864
prayas2409 <prayas.hemraj.pagade@gmail.com>
2020-12-08 19:08:56 +0530

    [Prayas] Refactored: Comments and kept at suitable places

--- snake_game.py
+++ snake_game.py
@@ -2,20 +2,22 @@ import pygame
 
 window_height = 500 # as we'll have square so height = width hence 1 param is fine
 rows = 20
-black_color = (0,0,0)
-white_color = (255,255,255)
+black_color = (0,0,0) # 0 0 0 is for black
+white_color = (255,255,255) # 255 255 255 is for white
 
 def draw_grids(max_width,num_of_rows,window):
-    gap_between_boxes = max_width // rows # // to get int
+    size_of_box = max_width // rows # // to get int
     x = y = 0 # initialize x and y to 0
     for _ in range(rows):
-        x = x + gap_between_boxes
-        y = y + gap_between_boxes
-        pygame.draw.line(window,white_color,(x,0), (x,max_width)) # drawing lines from x axis till max width
+        # increasing the x & y axis co-ordinates by size of each cube
+        x = x + size_of_box 
+        y = y + size_of_box
+        # drawing lines from x & y axis till max width to create grid /boxes
+        pygame.draw.line(window,white_color,(x,0), (x,max_width)) 
         pygame.draw.line(window,white_color,(0,y), (max_width,y))
 
 def redraw_window(window):
-    window.fill(black_color) # 0 0 0 is for black 
+    window.fill(black_color)  
     draw_grids(window_height,rows,window)
     pygame.display.update()
 
@@ -26,7 +28,5 @@ def main():
         pygame.time.delay(50) # 50 ms delay so that app does not run too fast
         clock.tick(10) # 10 fps, snake moves 10 blocks per sec
         redraw_window(window)
-        pass
-
 
 main()
