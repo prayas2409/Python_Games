f46d28598137e8aab6ffe7dcb10959d1a68fd5b7
prayas2409 <prayas.hemraj.pagade@gmail.com>
2020-12-12 13:17:38 +0530

    [Prayas] Added: Head is moving by default in right direction

--- snake_game.py
+++ snake_game.py
@@ -8,9 +8,12 @@ red_color = (255,0,0)
 
 class Cube:
 
-    def __init__(self,start,color):
+    def __init__(self,start,color,dirnx=1,dirny=0):
+        # Added default direction for cube it'll move in right direction by default as x=1, y=0
         self.pos = start
         self.color = color
+        self.dirnx = dirnx
+        self.dirny = dirny
 
     def draw(self,window, eyes=False):
         # eyes as true/false only for head it'll be sent true and for rest body it's false
@@ -29,12 +32,23 @@ class Cube:
             # Drawing actual eyes
             pygame.draw.circle(window, black_color, eye_one, radius)
             pygame.draw.circle(window, black_color, eye_two, radius)
-       
+
+    def move(self,dirnx,dirny):
+        # getting directions and updating the position
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
 
 class Snake:
 
     def __init__(self, color, pos):
         self.head = Cube(pos,color)
+        self.dirnx = 0
+        self.dirny = 1
+    
+    def move(self):
+        cube = self.head
+        cube.move(cube.dirnx,cube.dirny)
 
     def draw(self, window):
         self.head.draw(window,eyes=True) # Drawing head of snake in window
@@ -54,7 +68,8 @@ snake = Snake(red_color,(10,10)) # Send color and start position as x,y for snak
 
 def redraw_window(window):
     window.fill(black_color)
-    snake.draw(window)  
+    snake.draw(window)
+    snake.move()
     draw_grids(window_height,rows,window)
     pygame.display.update()
 
