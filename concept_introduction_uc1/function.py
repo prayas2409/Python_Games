04c307038bc0c8493b3bb353f6975cb5b361a392
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-01-15 15:17:14 +0530

    [Prayas] Added: Code to declare and call methods

---
+++ concept_introduction_uc1/function.py
@@ -0,0 +1,13 @@
+def default_printer():
+    print("Hi Prayas I can't change name")
+
+def name_printer(name):
+    # this method will print what ever name we pass to it
+    print("Hi",name)
+
+if __name__ == "__main__":
+    #call method without any parameters
+    default_printer()
+    company_name = "Brigelabz"
+    # pass company name to name printer
+    name_printer(company_name)
