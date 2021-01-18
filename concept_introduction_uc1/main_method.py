f8e8ae6fd89bd84917c16becf547d62cdff97f2f
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-01-18 14:48:00 +0530

    [Prayas] Added: show different calls to main method

--- concept_introduction_uc1/main_method.py
+++ concept_introduction_uc1/main_method.py
@@ -1,2 +1,13 @@
-if __name__ == "__main__":
-    print("Hiii! I am main method")
+
+def main():
+    print("In the main function")
+
+# bad practice
+print("calling main from outside")
+main()
+
+    # good practice
+if __name__ == "__main__": 
+    # checks if running current file only then executes below code
+    print("calling main method from inside")
+    main() 
