323dc0eb6a3edd7aeebbae1f9169ab4749e94356
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-01-30 01:13:04 +0530

    [Prayas] Added: Commwnts for better understanding of pass

--- concept_introduction_uc1/pass_keyword.py
+++ concept_introduction_uc1/pass_keyword.py
@@ -1,5 +1,6 @@
 def do_nothing():
-    # this gives a method bit it'll do nothing
+    # this gives a method that it'll do nothing
+    # As it'll do a pass that means skip what is below me in current scope
     pass
 
 do_nothing()
@@ -7,6 +8,7 @@ print("called do_nothing which did nothing")
 
 demo_var = 10
 if demo_var == 10:
+    # as demo var is equal to 10 it'll not do anything
     pass
 else:
     print("not equal to 10")
@@ -14,4 +16,5 @@ else:
 print("Executed if else")
 
 while demo_var==10:
+    # as it'll always be true it'll be running continously but nothing would be shown
     pass
