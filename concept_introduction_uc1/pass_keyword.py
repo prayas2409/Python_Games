814a3cdbde0071742bb308861e3c53e31b05c1c7
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-01-30 01:08:51 +0530

    [Prayas] Added: Code to understand pass keyword

---
+++ concept_introduction_uc1/pass_keyword.py
@@ -0,0 +1,17 @@
+def do_nothing():
+    # this gives a method bit it'll do nothing
+    pass
+
+do_nothing()
+print("called do_nothing which did nothing")
+
+demo_var = 10
+if demo_var == 10:
+    pass
+else:
+    print("not equal to 10")
+
+print("Executed if else")
+
+while demo_var==10:
+    pass
