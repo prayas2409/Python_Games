583d732e15b0a127880547453a53467d8372f43d
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-01-29 22:56:32 +0530

    [Prayas] Added: Import fucntions declared in function.py file

---
+++ concept_introduction_uc1/imports.py
@@ -0,0 +1,5 @@
+import function #bring all the declarations from function file to this file
+
+# calling the methods defined in functions file
+function.name_printer("Bridgelabz Solutions")
+function.default_printer()
